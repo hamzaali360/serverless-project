b248fccb6d59b307418409e30fc28632f016085e
Hamza Ali <hamali1708@gmail.com>
2021-07-09 18:52:41 -0500

    update

--- Week1Step4/index.js
+++ Week1Step4/index.js
@@ -2,13 +2,19 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const password = req.query.password || req.body.password
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    let your_response = "";
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: password
-    };
+    if(password == "letmein"){
+        your_response = "Access granted.";
+    }
+    else{
+        your_response = "Access denied.";
+    }
+
+context.res = {
+    // status: 200, /* Defaults to 200 */
+    
+    body: your_response
+};
+ 
 }
