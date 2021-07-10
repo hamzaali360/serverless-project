ac12347a9f237587a2adf8bb94a42b486f86c32e
Hamza Ali <hamali1708@gmail.com>
2021-07-10 16:02:03 -0500

    updated the twocatz function to return two cats and two random names

--- Week1Step4/index.js
+++ Week1Step4/index.js
@@ -1,9 +1,16 @@
 const fetch = require('node-fetch')
 
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+function generate_name(){
+    var names = ["Shreya", "Emily", "Fifi", "Beau", "Evelyn", "Julia", "Daniel", "Fardeen"]
+    var random_number = Math.floor(names.length * Math.random());
+    var random_name = names[random_number];
 
-    let resp = await fetch("https://cataas.com/cat/cute/says/Serverless", {
+    return random_name;
+}
+
+function generate_cat(){
+    let endpoint = "https://cataas.com/cat/cute/says/Bitcamp"
+    let resp = await fetch(endpoint, {
         method: 'GET'
     });
     
@@ -12,24 +19,38 @@ module.exports = async function (context, req) {
     // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob
     
     base64data = Buffer.from(data).toString('base64')
-//put what you want to turn into base64 inside "originaldata"
-//"originaldata" will be encoded in base64.
 
+    return base64data;
+}
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
 
-    // const password = req.query.password || req.body.password
-    // let your_response = "";
+    //let endpoint = "https://cataas.com/cat/cute/says/Bitcamp"
 
-    // if(password == "letmein"){
-    //     your_response = "Access granted.";
-    // }
-    // else{
-    //     your_response = "Access denied.";
-    // }
+    let name1 = generate_name();
+    let name2 = generate_name();
+    
+
+    // let resp1 = await fetch(endpoint, {
+    //     method: 'GET'
+    // });
+    
+    // let data1 = await resp1.arrayBuffer()
+    // // we need to receive it as a buffer since this is an image we are receiving from the API
+    // // Buffer?? https://developer.mozilla.org/en-US/docs/Web/API/Blob
+    
+    // base64data1 = Buffer.from(data).toString('base64')
+    let base64data1 = generate_cat();
+    let base64data2 = generate_cat();
 
 context.res = {
     // status: 200, /* Defaults to 200 */
     
-    body: base64data
+    body: {
+        cat1: base64data1,
+        cat2: base64data2,
+        names: [name1, name2]
+    }
 };
  
 }
