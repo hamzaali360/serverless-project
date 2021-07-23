d8e52406a743f6090b7e1952e1fcfd1c5a4b7729
Hamza Ali <hamali1708@gmail.com>
2021-07-23 17:01:56 -0500

    fix issues

--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -1,5 +1,5 @@
-const multipart = requre('parse-multipart');
-const fetch = requre('node-fetch');
+const multipart = require('parse-multipart');
+const fetch = require('node-fetch');
 
 const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
 const { BlobServiceClient } = require("@azure/storage-blob");
@@ -18,10 +18,11 @@ try {
     if (filetype == "image/png") {
         ext = "png";
     } else if (filetype == "image/jpeg") {
-        ext = "jpeg";
-    } else if (filetype == "image/jpg") {
-        ext = "jpg"
-    } else {
+        ext = "jpg";
+    } //else if (filetype == "image/jpg") {
+    //     ext = "jpg"
+    // } 
+    else {
         username = "invalidimage"
         ext = "";
     }
@@ -45,7 +46,7 @@ async function uploadFile(parsedBody, ext, password){
     const containerName = "images";
     const containerClient = blobServiceClient.getContainerClient(containerName);    // Get a reference to a container
 
-    const blobName = password + '.' + ext;    // Create the container
+    const blobName = password + "." + ext;    // Create the container
     const blockBlobClient = containerClient.getBlockBlobClient(blobName); // Get a block blob client
 
     const uploadBlobResponse = await blockBlobClient.upload(parsedBody[0].data, parsedBody[0].data.length);
